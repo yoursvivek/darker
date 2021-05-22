eccbce4ed29c0fac53c81d035e42464aee3477e5
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2021-05-22 22:43:00 +0300

    Prefer `@pytest.mark.kwparametrize(...)`
    It replaces `@pytest.mark.parametrize`. Installed from the
    `pytest-kwparametrize` package. Use it when there are more
    than two parametrization arguments.

--- src/darker/tests/test_black_diff.py
+++ src/darker/tests/test_black_diff.py
@@ -6,32 +6,37 @@ from darker.black_diff import BlackArgs, read_black_config, run_black
 from darker.utils import TextDocument
 
 
-@pytest.mark.parametrize(
-    "config_path, config_lines, expect",
-    [
-        (None, ['line-length = 79'], {'line_length': 79}),
-        ("custom.toml", ['line-length = 99'], {'line_length': 99}),
-        (
-            "custom.toml",
-            ['skip-string-normalization = true'],
-            {'skip_string_normalization': True},
-        ),
-        (
-            "custom.toml",
-            ['skip-string-normalization = false'],
-            {'skip_string_normalization': False},
-        ),
-        ("custom.toml", ["target-version = ['py37']"], {}),
-        ("custom.toml", ["include = '\\.pyi$'"], {}),
-        ("custom.toml", ["exclude = '\\.pyx$'"], {}),
-    ],
+@pytest.mark.kwparametrize(
+    dict(
+        config_path=None, config_lines=["line-length = 79"], expect={"line_length": 79}
+    ),
+    dict(
+        config_path="custom.toml",
+        config_lines=["line-length = 99"],
+        expect={"line_length": 99},
+    ),
+    dict(
+        config_path="custom.toml",
+        config_lines=["skip-string-normalization = true"],
+        expect={"skip_string_normalization": True},
+    ),
+    dict(
+        config_path="custom.toml",
+        config_lines=["skip-string-normalization = false"],
+        expect={"skip_string_normalization": False},
+    ),
+    dict(
+        config_path="custom.toml", config_lines=["target-version = ['py37']"], expect={}
+    ),
+    dict(config_path="custom.toml", config_lines=["include = '\\.pyi$'"], expect={}),
+    dict(config_path="custom.toml", config_lines=["exclude = '\\.pyx$'"], expect={}),
 )
 def test_black_config(tmpdir, config_path, config_lines, expect):
     tmpdir = Path(tmpdir)
     src = tmpdir / "src.py"
     toml = tmpdir / (config_path or "pyproject.toml")
 
-    toml.write_text("[tool.black]\n{}\n".format('\n'.join(config_lines)))
+    toml.write_text("[tool.black]\n{}\n".format("\n".join(config_lines)))
 
     config = read_black_config(src, config_path and str(toml))
     assert config == expect
